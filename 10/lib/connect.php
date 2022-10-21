5069c902b0a2bc248a4aa7900694ef20bd6ed499
Vlad <v.dutchak@365.softlist.ua>
2022-10-21 22:01:10 +0300

    Fix

--- 10/lib/connect.php
+++ 10/lib/connect.php
@@ -1,9 +1,9 @@
 <?php 
 $config = json_decode(file_get_contents("../config.json"))->sql_config;
-$servername = $config->servename;
-$username = $config->username;
-$password = $config->password;
-$database = $config->database;
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$database = "phplab";
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
